3574b803e3c7e902aeb706b68109a0b1b6f752fd
faran4engg <faran4engg@gmail.com>
2020-12-18 23:29:13 +0100

    more cleanup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,7 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
-import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import { GlobalStyle } from 'styles/global-styles';
-
 import { HomePage } from './containers/HomePage/Loadable';
-import { NotFoundPage } from './components/NotFoundPage/Loadable';
 
 export function App() {
   return (
@@ -28,9 +16,8 @@ export function App() {
 
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
+        {/* <Route component={NotFoundPage} /> */}
       </Switch>
-      <GlobalStyle />
     </BrowserRouter>
   );
 }
