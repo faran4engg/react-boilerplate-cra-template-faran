d47e8e47d4914fe30b8abff2e0aa740d16452381
faran4engg <faran4engg@gmail.com>
2020-12-18 21:59:58 +0100

    initial commit

---
+++ src/types/RootState.ts
@@ -0,0 +1,11 @@
+import { LolContainerState } from 'app/containers/LolContainer/types';
+// [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
+
+/* 
+  Because the redux-injectors injects your reducers asynchronously somewhere in your code
+  You have to declare them here manually
+*/
+export interface RootState {
+  lolContainer?: LolContainerState;
+  // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
+}
